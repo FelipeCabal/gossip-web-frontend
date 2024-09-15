d0d14e6fccdfa8295586b6c44e1c93d3f9238651
1Nath00 <nataliagomezdelacruz1424@gmail.com>
2024-09-15 12:07:10 -0500

    Creacion router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-function App() {
 
+const Login = () => {
+  return <h1>Holaaaa</h1>
+}
+const Register = () => {
+  return <h2>Registro</h2>
+}
+
+const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '',
+      element: <Login/>
+    },
+    {
+      path: '/register',
+      element:<Register/>
+      
+    }
+  ])
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
