7228c8c5eba7d60e0e4d60d43528f7e373ec7cb0
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2024-09-23 11:08:07 -0500

    ajuste de navbar

--- src/partials/navbar.jsx
+++ src/partials/navbar.jsx
@@ -8,13 +8,10 @@ import imgNotification from "../assets/icons/NOTIFICATIONS.png"
 import imgBuho from "../assets/icons/buho.png"
 
 const Navbar = () => {
-
-
     return (
         <header>
-
-            <div className="w-full h-17 flex items-center justify-center">
-                <div className="w-full h-20 text-slate-950 flex items-center justify-items-start">
+            <nav className="w-full h-17 flex justify-center items-center flex-col sm:flex-row">
+                <div className="w-full h-20 text-slate-950 flex items-center justify-center sm:justify-start">
                     <div className="flex flex-col items-center justify-center p-2">
                         <img src={img} alt="Gossip-web" className="max-w-full w-24 -mb-2" />
                         <h1 className="-m-1 -mt-3.5 px-5">Username</h1>
@@ -23,43 +20,33 @@ const Navbar = () => {
                 <div className="w-full h-20 text-slate-950 flex items-center justify-center">
                     <h1 className="text-3xl font-bold">Gossip-Web</h1>
                 </div>
-                <div className="w-full h-20  text-slate-950 flex items-center justify-end">
+                <div className="w-full h-20 text-slate-950 flex items-center justify-center sm:justify-end">
                     <img src={imgBuho} alt="Gossip-web" className="max-w-full w-24 mb-1" />
                 </div>
-            </div>
+            </nav>
 
-            <nav className="w-full h-16 bg-[#6f9ceb] border-[#306bac] border border-4">
-                <div className="w-full h-14  flex justify-center items-center text-white px-20 py-5">
-                    <div className="w-full flex items-center justify-between font-normal px-4">
-                        <Link to="/home">
-                            <a href="/home" className="flex items-center space-x-2 hover:text-gray-300">
-                                <img src={imgHome} alt="Homepage" className=" max-w-full w-12" />
-                            </a>
+            <nav className="w-full h-16 bg-[#6f9ceb] border-[#306bac] border-4">
+                <div className="w-full h-14 flex justify-center items-center text-white px-4 sm:px-20 py-5">
+                    <div className="w-full flex items-center justify-between font-normal px-2 sm:px-4 space-x-4">
+                        <Link to="/home" className="flex-1 flex items-center justify-center hover:text-gray-300">
+                            <img src={imgHome} alt="Homepage" className="max-w-full w-8 sm:w-12" />
                         </Link>
-                        <Link to="/notifications">
-                            <a href="/notifications" className="flex items-center space-x-2 hover:text-gray-300">
-                                <img src={imgNotification} alt="Notificaciones" className=" max-w-full w-12" />
-                            </a>
+                        <Link to="/search" className="flex-1 flex items-center justify-center hover:text-gray-300">
+                            <img src={imgBuscar} alt="Buscar" className="max-w-full w-8 sm:w-12" />
                         </Link>
-                        <Link to="/communities">
-                            <a href="/communities" className="flex items-center space-x-2 hover:text-gray-300">
-                                <img src={imgCommunity} alt="Comunidades" className=" max-w-full w-12" />
-                            </a>
+                        <Link to="/communities" className="flex-1 flex items-center justify-center hover:text-gray-300">
+                            <img src={imgCommunity} alt="Comunidades" className="max-w-full w-8 sm:w-12" />
                         </Link>
-                        <Link to="/messaging">
-                            <a href="/messaging" className="flex items-center space-x-2 hover:text-gray-300">
-                                <img src={imgMensajes} alt="Mensajes" className=" max-w-full w-12" />
-                            </a>
+                        <Link to="/messaging" className="flex-1 flex items-center justify-center hover:text-gray-300">
+                            <img src={imgMensajes} alt="Mensajes" className="max-w-full w-8 sm:w-12" />
                         </Link>
-                        <Link to="/search">
-                            <a href="/search" className="flex items-center space-x-2 hover:text-gray-300">
-                                <img src={imgBuscar} alt="Buscar" className=" max-w-full w-12" />
-                            </a>
+
+                        <Link to="/notifications" className="flex-1 flex items-center justify-center hover:text-gray-300">
+                            <img src={imgNotification} alt="Notificaciones" className="max-w-full w-8 sm:w-12" />
                         </Link>
                     </div>
                 </div>
             </nav>
-
         </header>
     )
 }
