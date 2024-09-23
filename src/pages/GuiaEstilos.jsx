46b9ad478b0b013766ae464001196b781de4da2f
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2024-09-23 10:36:41 -0500

    ajustes de el grid del layout y volverlo responsive

--- src/pages/GuiaEstilos.jsx
+++ src/pages/GuiaEstilos.jsx
@@ -1,21 +1,7 @@
 export function GuiaEstilos() {
     return (
-        <div>
-            <section className='introduccion'>
-                <h1 className="text-[25px]">Guía de estilos</h1>
-                <h2>Introduccion</h2>
-                <p>La Guía de Estilos tiene como objetivo proporcionar un conjunto de reglas y componentes predefinidos que permiten mantener la consistencia visual y de comportamiento a lo largo de toda la aplicación. Al utilizar esta guía, los desarrolladores y diseñadores pueden asegurarse de que la interfaz de usuario sea coherente, accesible y fácil de mantener, independientemente de la escala o el tamaño del proyecto.</p>
-            </section>
-            <section className='tipografia'>
-                <h2>Tipografía</h2>
-                <p>La tipografía es un elemento clave en el diseño de interfaces. La elección de una fuente adecuada puede mejorar la legibilidad y la accesibilidad de la aplicación. A continuación se presentan las fuentes recomendadas para su uso en la aplicación:</p>
-                <ul>
-                    <li>Nerco One: Nerko One fue creada por el diseñador Ellograph Studio y está disponible en Google Fonts. Esta va a ser usada para los titulos principales con tipografia base de 16 px</li>
-                    <li>Robot Candense: Fue desarrollada originalmente para Google como parte de su sistema operativo Android y lanzada en 2011 y se utiliza para los títulos secundarios con tipografia base de 16 px.</li>
-                    <li>Inter:  Es una fuente de código abierto diseñada principalmente para pantallas y lectura en interfaces digitales y va a ser usada para textos generales con tipografia base de 16 px. </li>
-                </ul>
-            </section>
-            <section className='colores'>
+        <div className="containerHome">
+            <div >
                 <h2>Colores</h2>
                 <p>La elección de una paleta de colores adecuada es fundamental para el diseño de interfaces. Los colores pueden transmitir emociones y crear una atmósfera única en la aplicación. A continuación se presentan los colores recomendados para su uso en la aplicación:</p>
                 <ul>
@@ -31,7 +17,21 @@ export function GuiaEstilos() {
                         <li className='bg-[#98b9f2]'>Secundario: #98b9f2</li>
                     </ul>
                 </ul>
-            </section>
+            </div>
+            <div>
+                <h1 className="text-[25px]">Guía de estilos</h1>
+                <h2>Introduccion</h2>
+                <p>La Guía de Estilos tiene como objetivo proporcionar un conjunto de reglas y componentes predefinidos que permiten mantener la consistencia visual y de comportamiento a lo largo de toda la aplicación. Al utilizar esta guía, los desarrolladores y diseñadores pueden asegurarse de que la interfaz de usuario sea coherente, accesible y fácil de mantener, independientemente de la escala o el tamaño del proyecto.</p>
+
+                <h2>Tipografía</h2>
+                <p>La tipografía es un elemento clave en el diseño de interfaces. La elección de una fuente adecuada puede mejorar la legibilidad y la accesibilidad de la aplicación. A continuación se presentan las fuentes recomendadas para su uso en la aplicación:</p>
+                <ul>
+                    <li>Nerco One: Nerko One fue creada por el diseñador Ellograph Studio y está disponible en Google Fonts. Esta va a ser usada para los titulos principales con tipografia base de 16 px</li>
+                    <li>Robot Candense: Fue desarrollada originalmente para Google como parte de su sistema operativo Android y lanzada en 2011 y se utiliza para los títulos secundarios con tipografia base de 16 px.</li>
+                    <li>Inter:  Es una fuente de código abierto diseñada principalmente para pantallas y lectura en interfaces digitales y va a ser usada para textos generales con tipografia base de 16 px. </li>
+                </ul>
+            </div>
+
         </div>
     )
 }
