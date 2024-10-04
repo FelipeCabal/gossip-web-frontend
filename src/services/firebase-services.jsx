526bf427ec839ae4e4941b3a33ec251daefe805d
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-10-04 12:05:04 -0500

    firebase-services

---
+++ src/services/firebase-services.jsx
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA8lOrLwD2_FPL6u8sMZYyfIPHLJ7vsDv0",
+    authDomain: "gossipweb-37abf.firebaseapp.com",
+    projectId: "gossipweb-37abf",
+    storageBucket: "gossipweb-37abf.appspot.com",
+    messagingSenderId: "118148093161",
+    appId: "1:118148093161:web:d11d2bd2a0c49c7ffdcc78",
+    measurementId: "G-2H2JT8KV38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const uploadFile = async (asset, onProgress) => {
+    return new Promise((resolve, reject) => {
+        const storageRef = ref(storage, `uploads/${asset.type}/${asset.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, asset.file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                onProgress(progress);
+            },
+            (error) => {
+                reject(error);
+            },
+            () => {
+                getDownloadURL(storageRef).then(async (url) => {
+                    return await /*Aqui se hace la peticion patch para actualizar*/registerFile(url, asset).then(() => {
+                        resolve();
+                    });
+
+                });
+            }
+        );
+    });
+};
+
+export { uploadFile };
