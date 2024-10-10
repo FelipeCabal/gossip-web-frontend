532ff14daca79bb66c16dc7dd39cf2dfdfecfce9
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-10-09 19:12:39 -0500

    Ajuste de el servicio de firebase y agregue el metodo deletfile

--- src/services/firebase-services.jsx
+++ src/services/firebase-services.jsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import axios from "axios";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const uploadFile = async (asset, onProgress) => {
+const uploadFile = async (asset, onProgress, idOwner) => {
     return new Promise((resolve, reject) => {
         const storageRef = ref(storage, `uploads/${asset.type}/${asset.name}`);
         const uploadTask = uploadBytesResumable(storageRef, asset.file);
@@ -36,7 +37,7 @@ const uploadFile = async (asset, onProgress) => {
             },
             () => {
                 getDownloadURL(storageRef).then(async (url) => {
-                    return await /*Aqui se hace la peticion patch para actualizar*/registerFile(url, asset).then(() => {
+                    return await axios.patch(`${process.env.REACT_APP_API, asset.type}/${idOwner}`, { imagen: url }).then(() => {
                         resolve();
                     });
 
@@ -45,5 +46,9 @@ const uploadFile = async (asset, onProgress) => {
         );
     });
 };
+const deleteFile = async (asset) => {
+    const storageRef = ref(storage, `uploads/${asset.type}/${asset.name}`);
+    return await deleteObject(storageRef)
+};
 
-export { uploadFile };
+export { deleteFile, uploadFile };
