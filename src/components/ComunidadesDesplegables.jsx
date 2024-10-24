7bea92b9d2442dd3dee637cab2562c2aa9753318
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2024-10-24 11:03:05 -0500

    mejoras de la vista de comunidades

--- src/components/ComunidadesDesplegables.jsx
+++ src/components/ComunidadesDesplegables.jsx
@@ -12,16 +12,13 @@ const Comunidades = () => {
     ];
 
     return (
-        <div className="bg-white p-8 rounded-lg w-auto">
+        <div className="rounded-lg w-auto">
             <h2 className="text-2xl font-bold text-center mb-6">COMUNIDADES</h2>
-            <div className="space-y-6 pt-1">
+            <div className=" mt-1 w-auto -mr-14">
                 {comunidades.map((comunidad) => (
-                    <div
-                        key={comunidad.id}
-                        className="flex items-center border-b-2 justify-between border-b-[#918ef4] pb-1 w-full"
-                    >
-                        <img src={icono} alt="Icono de búho" className="w-12 " />
-                        <Link className="ml-4 text-lg text-center text-[#306bac] font-bold flex-1">{comunidad.nombre}</Link>
+                    <div key={comunidad.id} className="flex items-center border-b-2 justify-around  border-b-[#918ef4] w-full -ml-10">
+                        <img src={icono} alt="Icono de búho" className="w-32 " />
+                        <Link className=" text-lg text-center text-[#306bac] font-bold flex-1 mr-3">{comunidad.nombre}</Link>
                     </div>
                 ))}
             </div>
