b01b96eb567e3681dd4dc0cf45591b842e421b16
1Nath00 <nataliagomezdelacruz1424@gmail.com>
2024-12-10 13:13:16 -0500

    ajustes

--- src/components/PublicacionesHome/PublicacionHome.jsx
+++ src/components/PublicacionesHome/PublicacionHome.jsx
@@ -49,7 +49,6 @@ export function PublicacionHome({ userName, img, texto, perfil, esAnonimo, postI
 
   const currentPath = window.location.pathname;
 
-
   return (
     <>
       <div className="w-full flex justify-center place-items-center">
@@ -76,6 +75,18 @@ export function PublicacionHome({ userName, img, texto, perfil, esAnonimo, postI
               </Link>
               <section className="bottom-0 right-0 flex space-x-3 p-4 w-full">
                 <div className="absolute bottom-0 right-0 flex justify-end space-x-3 pt-6 pb-4 pr-4 w-full">
+                  <button>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                      fill="none" viewBox="0 0 24 24"
+                      strokeWidth={1.5} stroke="currentColor"
+                      className="size-10">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+
+                  </button>
                   <button onClick={handleClick}>
                     <svg
                       data-slot="icon"
