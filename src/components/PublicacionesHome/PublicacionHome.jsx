2dad9c233921011374d03fc63b5d03ed3a707438
1Nath00 <nataliagomezdelacruz1424@gmail.com>
2024-12-10 22:47:15 -0500

    funcionalidad boton elimar publicacion

--- src/components/PublicacionesHome/PublicacionHome.jsx
+++ src/components/PublicacionesHome/PublicacionHome.jsx
@@ -1,12 +1,37 @@
 import { useEffect, useState } from "react";
 import anonimo from '../../assets/icons/Buho.png';
 import { FormComment } from "../Comentarios/FormComment";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../../providers/AuthProvider";
 import { useRefresh } from "../../providers/RefreshProvider";
 
-export function PublicacionHome({ userName, img, texto, perfil, esAnonimo, postId, }) {
+function ConfirmModal({ mensaje, onConfirm, onCancel }) {
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[9999]"
+    >
+      <div className="bg-white p-6 rounded shadow-lg z-[10000]">
+        <p className="font-roboto text-lg mb-4">{mensaje}</p>
+        <div className="flex justify-end space-x-4">
+          <button
+            className="btn btn-4"
+            onClick={onConfirm}>
+            Aceptar
+          </button>
+          <button
+            className="btn btn-3"
+            onClick={onCancel}>
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export function PublicacionHome({ userName, img, texto, perfil, esAnonimo, postId, userId }) {
   const { usuario } = useAuth()
   const [expandir, setExpandir] = useState(false); // Controla si la publicación está extendida
   const toggleExpandir = () => setExpandir(!expandir); // Alterna el estado
@@ -56,9 +81,24 @@ export function PublicacionHome({ userName, img, texto, perfil, esAnonimo, postI
     pathDireccion = '/' + pathParts[1] + '/' + pathParts[2]
   }
 
-  useEffect(() => {
+  const [showModal, setShowModal] = useState(false);
 
-  })
+  const eliminarPublicacion = () => {
+    axios.delete(`${process.env.REACT_APP_API}/posts/${postId}`)
+      .then(response => {
+        console.log('Publicación eliminada:', response.data);
+        setRefresh(true);
+        setShowModal(false);
+      })
+      .catch(error => {
+        console.error('Error al eliminar la publicación:', error);
+        setShowModal(false);
+      });
+  };
+
+  const manejarEliminar = () => setShowModal(true);
+
+
 
   return (
     <>
@@ -70,12 +110,22 @@ export function PublicacionHome({ userName, img, texto, perfil, esAnonimo, postI
                 <img src={fotoPerfil} alt="perfil" className="w-20 h-20 rounded-full object-cover" />
                 <span className="flex items-center justify-center text-xl font-bold">@{nombre}</span>
               </div>
-              <button className="flex items-start pt-4 pr-2 pb-2 gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                </svg>
+              {
+                userId == usuario.id ? <div>
+                  <button className="pr-2 pt-3" onClick={manejarEliminar}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                  </svg>
+                  </button>
+                  {showModal && (
+                    <ConfirmModal
+                      mensaje="¿Estás seguro de que deseas eliminar esta publicación?"
+                      onConfirm={eliminarPublicacion}
+                      onCancel={() => setShowModal(false)}
+                    />
+                  )}
+                </div> : <></>
+              }
 
-              </button>
             </div>
             <aside className="flex-col flex justify-center items-center relative">
               <Link to={pathDireccion + '/post/' + postId}>
@@ -93,18 +143,22 @@ export function PublicacionHome({ userName, img, texto, perfil, esAnonimo, postI
 
               <section className="bottom-0 right-0 flex space-x-3 p-4 w-full">
                 <div className="absolute bottom-0 right-0 flex justify-end space-x-3 pt-6 pb-4 pr-4 w-full">
-                  <button>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                      fill="none" viewBox="0 0 24 24"
-                      strokeWidth={1.5} stroke="currentColor"
-                      className="size-10">
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                    </svg>
+                  {
+                    userId == usuario.id ? <button>
+                      <svg xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24"
+                        strokeWidth={1.5} stroke="currentColor"
+                        className="size-10">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                      </svg>
+                    </button>
+                      : <></>
+                  }
 
-                  </button>
+
                   <button onClick={handleClick}>
                     <svg
                       data-slot="icon"
