2f134cbdd04d55d564bcb483ab89deb75a350aa2
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-12-15 01:15:59 -0500

    finish responsive chats page

--- src/components/Chat/ChatComponent.jsx
+++ src/components/Chat/ChatComponent.jsx
@@ -157,7 +157,7 @@ const ChatComponent = () => {
                             {/* Encabezado del chat */}
                             <div className="flex justify-between items-center border-b-2 border-slate-900 p-2">
                                 <div className='flex items-center gap-2'>
-                                    <div onClick={() => navigate("/chats")}>
+                                    <div onClick={() => navigate("/chats")} className='back-chats'>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-10 cursor-pointer">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
                                         </svg>
